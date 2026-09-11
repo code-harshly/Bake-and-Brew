9e30a345bb1fd6c5075bfcb05a344da3ddb7fe46
Harsh Mishra <hmxharshmishra@gmail.com>
2026-09-11 15:28:38 +0530

    Load Vercel API application lazily

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,9 @@
-import { app } from '../server.ts';
+import type { Request, Response } from 'express';
 
-export default function handler(req: Parameters<typeof app>[0], res: Parameters<typeof app>[1]) {
+let appPromise: Promise<(req: Request, res: Response) => unknown> | undefined;
+
+export default async function handler(req: Request, res: Response) {
+  appPromise ??= import('../server.ts').then(({ app }) => app);
+  const app = await appPromise;
   return app(req, res);
 }
