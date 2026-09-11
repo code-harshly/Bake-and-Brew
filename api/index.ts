e26c2640cf7231bf2c5cc48193742d3f7606dbd6
Harsh Mishra <hmxharshmishra@gmail.com>
2026-09-11 15:20:00 +0530

    Fix Vercel API function routing

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
 import { app } from '../server.ts';
 
-export default app;
+export default function handler(req: Parameters<typeof app>[0], res: Parameters<typeof app>[1]) {
+  return app(req, res);
+}
